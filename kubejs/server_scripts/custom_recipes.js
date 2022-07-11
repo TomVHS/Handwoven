bbb114c601f8346914ae93ed5c640e4ab0150494
Tom <104469473+TomVHS@users.noreply.github.com>
2022-07-10 23:31:36 -0400

    Delete kubejs directory

--- kubejs/server_scripts/custom_recipes.js
+++
@@ -1,7 +0,0 @@
-onEvent('recipes', event => {
-    // Add recipe for custom 'Rotten Leather' (x2 Rotten Flesh -> x1 Rotten Leather)
-    event.shapeless('kubejs:rotten_leather', ['minecraft:rotten_flesh','minecraft:rotten_flesh'])
-
-    // Add smoker recipe for turning 'Rotten Leather' into Leather
-    event.smoking('minecraft:leather','kubejs:rotten_leather')
-})
